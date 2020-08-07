02ee375d82ee0dd17e3d9e018e10882dd2aeaf41
Zenahr Barzani <zenmatica@gmail.com>
2020-08-08 01:38:15 +0200

    added generator code

--- src/frontend/generate.py
+++ src/frontend/generate.py
@@ -1,1 +1,28 @@
+from jinja2 import Environment, FileSystemLoader
+from json import load
+import json
+
+template_env = Environment(loader=FileSystemLoader(searchpath='./template'))
+template = template_env.get_template('template.html')
+
+with open('template/config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+with open('public/index.html', 'w') as output_file:
+    output_file.write(
+        template.render(
+            title=config['content']['title'],
+            greetingText=config['content']['greetingText'],
+            resumeText=config['content']['resumeText'],
+            firstName=config['content']['firstName'],
+            lastName=config['content']['lastName'],
+            github=config['socials']['github'],
+            linkedIn=config['socials']['linkedIn'],
+            email=config['socials']['email'],
+            socialIntroText=config['content']['socialIntroText'],
+            closingStatement=config['content']['closingStatement'],
+            js=""
+        )
+    )
+
 file:///
