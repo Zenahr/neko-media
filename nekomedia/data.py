4217c29fca6ee6df22936864810fef9f61c9958c
Zenahr <zenmatica@gmail.com>
2021-07-15 06:53:34 +0200

    add credit for video to image conversion code snippet.

--- nekomedia/data.py
+++ nekomedia/data.py
@@ -33,6 +33,7 @@ def get_folders():
     return folders
 
 def generate_preview_thumb(video_files_directory, output_loc='./static/thumbs'):
+    """"Credit: https://stackoverflow.com/a/49011190/12675239"""
     # TODO: don't use filename but rather directory name. Makes it easier to create url_for() references to thumbnails later on.
     video_file_name = None
     for entry in os.listdir(video_files_directory):
@@ -40,34 +41,15 @@ def generate_preview_thumb(video_files_directory, output_loc='./static/thumbs'):
             video_file_name = entry
             break
     file = os.path.join(video_files_directory, video_file_name)
-
     try:
         os.mkdir(output_loc)
     except OSError:
         pass
-    # Log the time
-    time_start = time.time()
-    # Start capturing the feed
     cap = cv2.VideoCapture(file)
-    # Find the number of frames
-    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    print ("Number of frames: ", video_length)
-    count = 0
-    print ("Converting video..\n")
-    # Start converting the video
     while cap.isOpened():
-        # Extract the frame
         ret, frame = cap.read()
         cv2.imwrite(output_loc + f"/{video_file_name}.jpg", frame)
         break # we only need ONE image per video for our preview.
-        count = count + 1
-        if (count > (video_length-1)):
-            time_end = time.time()
-            cap.release()
-            print ("Done extracting frames.\n%d frames extracted" % count)
-            print ("It took %d seconds forconversion." % (time_end-time_start))
-            break
-
 
     # first_level_folder_names = [ re.sub("[\(\[].*?[\)\]]", "", name).strip() for name in os.listdir(os.getenv('MEDIA_PATH')) if os.path.isdir(os.path.join(os.getenv('MEDIA_PATH'), name)) ]
     # full_path_to_first_level_folders = [f.path for f in os.scandir(os.getenv('MEDIA_PATH')) if f.is_dir()] # get fullpath to first level folders only.
